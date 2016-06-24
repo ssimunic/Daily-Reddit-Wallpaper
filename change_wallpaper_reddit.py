f6c99579938309c5bdd0df7a590d9a8505bef6e6
Silvio Simunic <silvio.simunic@gmail.com>
2016-06-24 23:59:45 +0200

    Update change_wallpaper_reddit.py

--- change_wallpaper_reddit.py
+++ change_wallpaper_reddit.py
@@ -5,23 +5,24 @@ import subprocess
 import requests
 import sys
 
-# Get Imgur ID of most upvoted wallpaper
-def get_top_image_id(subreddit):
+# Get image link of most upvoted wallpaper of the day
+def get_top_image(subreddit):
     for submission in subreddit.get_top_from_day(limit=10):
         url = submission.url
-        # Use only direct Imgur images
+        if url.endswith(".jpg"):
+            return url
+        # Imgur support
         if ("imgur.com" in url) and ("/a/" not in url):
             id = url.rsplit("/", 1)[1].rsplit(".", 1)[0]
-            # Return ID for the first valid link
-            return id
+            return "http://imgur.com/" + id + ".jpg"
+
 
 # Python Reddit Api Wrapper
 r = praw.Reddit(user_agent="Get top wallpaper from /r/wallpers by /u/ssimunic")
 subreddit = sys.argv[1] if len(sys.argv) > 1 else "wallpapers"
 
-# Get Imgur ID and full path
-topImageId = get_top_image_id(r.get_subreddit(subreddit))
-imageUrl = "http://imgur.com/" + topImageId + ".jpg"
+# Get top image path
+imageUrl = get_top_image(r.get_subreddit(subreddit))
 
 # Request image
 response = requests.get(imageUrl)
@@ -31,7 +32,7 @@ if response.status_code == 200:
     # Get home directory and location where image will be saved (default location for Ubuntu is used)
     homedir = os.path.expanduser('~')
     saveLocation = homedir + "/Pictures/Wallpapers/"
-    saveFileName = topImageId + ".jpg"
+    saveFileName = "wallpaper.jpg"
 
     # Write to disk
     with open(saveLocation + saveFileName, 'wb') as fo:
