5d01809213f85c3dfc449ba7569242933853be14
Guillermo Robles <guillerobles1995@gmail.com>
2016-06-25 10:10:12 +0200

    Add Desktop Environment autodetection

--- change_wallpaper_reddit.py
+++ change_wallpaper_reddit.py
@@ -27,6 +27,25 @@ def get_top_image(subreddit):
             id = url.rsplit("/", 1)[1].rsplit(".", 1)[0]
             return "http://imgur.com/" + id + ".jpg"
 
+# Get current Desktop Environment
+# Got from: http://stackoverflow.com/questions/2035657/what-is-my-current-desktop-environment
+def detect_desktop_environment():
+    desktop_environment = 'generic'
+    if os.environ.get('KDE_FULL_SESSION') == 'true':
+        desktop_environment = 'kde'
+    elif os.environ.get('GNOME_DESKTOP_SESSION_ID'):
+        desktop_environment = 'gnome'
+    elif os.environ.get('DESKTOP_SESSION') == 'mate':
+        desktop_environment = 'mate'
+    else:
+        try:
+            info = getoutput('xprop -root _DT_SAVE_MODE')
+            if ' = "xfce4"' in info:
+                desktop_environment = 'xfce'
+        except (OSError, RuntimeError):
+            pass
+    return desktop_environment
+
 # Python Reddit Api Wrapper
 r = praw.Reddit(user_agent="Get top wallpaper from /r/" + args.subreddit + " by /u/ssimunic")
 
@@ -55,7 +74,11 @@ if response.status_code == 200:
     # Check if OS is Linux or Windows, then execute command to change wallpaper
     platformName = platform.system()
     if platformName.startswith("Lin"):
-        os.system("gsettings set org.gnome.desktop.background picture-uri file://" + saveLocation)
+        desktopEnvironment = detect_desktop_environment();
+        if desktopEnvironment == 'gnome':
+            os.system("gsettings set org.gnome.desktop.background picture-uri file://" + saveLocation)
+        else:
+            print "Unsupported desktop environment"
     if platformName.startswith("Win"):
         SPI_SETDESKWALLPAPER = 20
         ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, saveLocation, 3)
