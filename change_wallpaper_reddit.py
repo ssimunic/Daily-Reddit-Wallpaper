07dcadb23d368cd9b9e69fefabe0b5fea157ad69
Łukasz Żarnowiecki <dolohow@outlook.com>
2018-03-20 17:01:25 +0100

    Do not assume all images are jpges

--- change_wallpaper_reddit.py
+++ change_wallpaper_reddit.py
@@ -99,16 +99,16 @@ def get_top_image(sub_reddit):
         url = submission.url
         # Strip trailing arguments (after a '?')
         url = re.sub(R"\?.*", "", url)
+        ret['type'] = url.split(".")[-1]
         if url.endswith(".jpg") or url.endswith(".png"):
             ret["url"] = url
-            return ret
         # Imgur support
         if ("imgur.com" in url) and ("/a/" not in url) and ("/gallery/" not in url):
             if url.endswith("/new"):
                 url = url.rsplit("/", 1)[0]
             id = url.rsplit("/", 1)[1].rsplit(".", 1)[0]
             ret["url"] = "http://i.imgur.com/{id}.jpg".format(id=id)
-            return ret
+        return ret
 
 
 def detect_desktop_environment():
@@ -179,9 +179,10 @@ if __name__ == '__main__':
         # Get home directory and location where image will be saved
         # (default location for Ubuntu is used)
         home_dir = os.path.expanduser("~")
-        save_location = "{home_dir}/{save_dir}/{subreddit}-{id}.jpg".format(home_dir=home_dir, save_dir=save_dir,
+        save_location = "{home_dir}/{save_dir}/{subreddit}-{id}.{image_type}".format(home_dir=home_dir, save_dir=save_dir,
                                                                             subreddit=subreddit,
-                                                                            id=image["id"])
+                                                                            id=image["id"],
+                                                                            image_type=image['type'])
 
         if os.path.isfile(save_location):
             sys.exit("Info: Image already exists, nothing to do, the program is" \
